9ccfcb9e945cda57e76864be827b832a5ed73434
Ender36046 <117245335+Ender36046@users.noreply.github.com>
2026-09-08 09:54:10 -0600

    Add searchbar logic

--- project/static/index.js
+++ project/static/index.js
@@ -3,6 +3,10 @@ const searchbar = document.getElementById("searchbar")
 let slug = ""
 
 searchbar.addEventListener("input", function(e){
-    slug = slug + e.target.value
+    slug = e.target.value
+    const baseURL = window.location.origin
+    if(e.key == "Enter"){
+        window.location.href = `${baseURL}/items/slug`
+    }
     console.log(slug)
 })
